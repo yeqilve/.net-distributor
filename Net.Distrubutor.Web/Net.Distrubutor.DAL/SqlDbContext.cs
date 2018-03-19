14c34d4f426d533ab1b6f76c4c33ed621438ff0e
shawn <shawn_skr@163.com>
2018-03-19 20:12:39 +0800

    test

---
+++ Net.Distrubutor.Web/Net.Distrubutor.DAL/SqlDbContext.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Net.Distrubutor.DAL
+{
+    public class SqlDbContext : DbContext, IDbContext
+    {
+
+        public SqlDbContext(DbContextOptions<SqlDbContext> connectString) : base(connectString)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            var typesToRegister = Assembly.GetExecutingAssembly().GetTypes()
+            .Where(type => !string.IsNullOrEmpty(type.Namespace))
+            .Where(type => type.BaseType != null && type.BaseType.IsGenericType &&
+                type.BaseType.GetGenericTypeDefinition() == typeof(SqlDbContext));
+            foreach (var type in typesToRegister)
+            {
+                dynamic configurationInstance = Activator.CreateInstance(type);
+                builder.Model.AddEntityType(configurationInstance);
+            }
+
+            base.OnModelCreating(builder); 
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var builder = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            var configuration = builder.Build();
+            string connectString = configuration.GetConnectionString("Conte");
+            optionsBuilder.UseMySql(connectString);
+        }
+
+        public new DbSet<TEntity> Set<TEntity>() where TEntity : BaseEntity
+        {
+            return base.Set<TEntity>();
+        }
+
+        public bool ProxyCreationEnabled { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool AutoDetectChangesEnabled { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void Detach(object entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ExecuteSqlCommand(string sql, bool doNotEnsureTransaction = false, int? timeout = null, params object[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<TEntity> ExecuteStoredProcedureList<TEntity>(string commandText, params object[] parameters) where TEntity : BaseEntity, new()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TElement> SqlQuery<TElement>(string sql, params object[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
